654312014cc116aae0cd5dcafd49e1eff2a136e8
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-08-28 06:18:23 -0400

    Create assignment-13

---
+++ assignments/lecture/gopractise.js
@@ -0,0 +1,16 @@
+var fullName1 = "john doe";
+var fullName2 = "Jane smith";
+var fullName3 = "bob Johnson";
+
+var name1 = fullName1.indexOf(" ");
+var firstLetterName = fullName1[0];
+var lastLetterFirstName = fullName1[name1 + 1];
+console.log(firstLetterName + lastLetterFirstName);
+
+console.log(Math.floor(Math.random() * 51) + 50);
+
+var range = Math.floor(Math.random() * 3) + 1;
+var a;
+if (range > 3 && range > 1) {
+    name = "Nora";
+}
