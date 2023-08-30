78cc704adad2d411c8059c25aa0d873a5042e06c
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-08-29 21:44:41 -0400

    Finish the lecture

--- assignments/lecture/gopractise.js
+++
@@ -1,8 +0,0 @@
-const { doubleouble } = require("./utilities");
-
-const sumEvenNumbers = (arr) => {
-    return arr.reduce((acc, num) => (double(num) ? acc * num : acc), 0);
-};
-
-// Code to use sumEvenNumbers
-console.log(sumEvenNumbers([1, 2, 3, 4, 5])); // Outputs 6
